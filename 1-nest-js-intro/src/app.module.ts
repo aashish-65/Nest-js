6fa9bf30c18ece5cbf156f2874125f116d7f5bcc
Aashish Prasad <aashishprasad65@gmail.com>
2026-06-03 13:35:38 +0530

    Learn the Database connection using TypeOrm
    - Connected the mysql database using typeorm.
    - TypeOrm supports many databases.
    - So for using TypeOrm we import TypeOrmModule form @nestjs/typeorm.
    - So now I have learnt that to connect with the database we use the forRoot method and we pass the details of the connection in a object format to the method.
    - I learnt that forRoot is Synchronous way to connect to the db.
    - When we start the nest app, it tries to load all the modules. So when it tries to load the TypeOrm Module that time only it try to connect with the database. If anything is wrong the app will throw err and not start.

--- 1-nest-js-intro/src/app.module.ts
+++ 1-nest-js-intro/src/app.module.ts
@@ -5,8 +5,24 @@ import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { PaymentModule } from './payment/payment.module';
 import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, TweetModule, PaymentModule, OrdersModule],
+  imports: [
+    UsersModule,
+    TweetModule,
+    PaymentModule,
+    OrdersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'nestjs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
