63d483fd50f2f13b67a366428428195e92742e2d
Aashish Prasad <aashishprasad65@gmail.com>
2026-06-04 22:01:20 +0530

    Learn about Pipes
    - Pipes in nextjs is fucntion which act as specialized type of middleware which runs brore the controller.
    - The main usecase of pipes are Data Transformation, Data Validation and Data Sanitization.
    - There are many build in pipes like ValidationPipe ParseIntPipe ParseFloatPipe ParseBoolPipe ParseArrayPipe ParseUUIDPipe ParseEnumPipe DefaultValuePipe ParseFilePipe ParseDatePipe.
    - The ValidationPipe and DefaultValuePipe show be created with the new keyword, rest can be used directly.

--- 1-nest-js-intro/src/users/users.controller.ts
+++ 1-nest-js-intro/src/users/users.controller.ts
@@ -1,27 +1,48 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 //http://localhost:3000/users
 @Controller('users')
-export class UsersController{
-    @Get()
-    getUsers(){
-        const usersService = new UsersService();
-        return usersService.getAllUsers();
-    }
+export class UsersController {
+  usersService: UsersService;
+  constructor() {
+    this.usersService = new UsersService();
+  }
+  @Get()
+  getUsers(
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit, page);
+    return this.usersService.getAllUsers();
+  }
 
-    @Get(':id')
-    getUserById(@Param('id') id: any){
-        const usersService = new UsersService();
-        return usersService.getUserById(+id); // By adding this + we are converting id to number.
-    }
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    const usersService = new UsersService();
+    console.log(typeof id, id);
+    return usersService.getUserById(id); // By adding this + we are converting id to number.
+  }
 
-    @Post()
-    createUser(){
-        const user = { id: 3, name: "merry", age:45, gender: 'female', isMarried: false};
-        const usersService = new UsersService();
-        const result = usersService.createUser(user);
-        return result;
-
-    }
+  @Post()
+  createUser() {
+    const user = {
+      id: 3,
+      name: 'merry',
+      age: 45,
+      gender: 'female',
+      isMarried: false,
+    };
+    const usersService = new UsersService();
+    const result = usersService.createUser(user);
+    return result;
+  }
 }
--- 1-nest-js-intro/src/users/users.service.ts
+++ 1-nest-js-intro/src/users/users.service.ts
@@ -1,23 +1,50 @@
-export class UsersService{
-    users: {id: number, name: string, age: number, gender: string, isMarried: boolean}[] = [
-        {
-            id: 1, name: 'jhon', age: 28, gender: 'male', isMarried: false
-        },
-        {
-            id: 2,name: 'mark', age: 32, gender: 'male', isMarried: true
-        }
-    ]
+export class UsersService {
+  users: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarried: boolean;
+  }[] = [
+    {
+      id: 1,
+      name: 'jhon',
+      age: 28,
+      gender: 'male',
+      isMarried: false,
+    },
+    {
+      id: 2,
+      name: 'mark',
+      age: 32,
+      gender: 'male',
+      isMarried: true,
+    },
+    {
+      id: 3,
+      name: 'maria',
+      age: 34,
+      gender: 'female',
+      isMarried: false,
+    },
+  ];
 
-    getAllUsers(){
-        return this.users;
-    }
+  getAllUsers() {
+    return this.users;
+  }
 
-    getUserById(id: number){
-        return  this.users.find(x => x.id === id);
-    }
+  getUserById(id: number) {
+    return this.users.find((x) => x.id === id);
+  }
 
-    createUser(user: {id: number, name: string, age: number, gender: string, isMarried: boolean}){
-        this.users.push(user);
-        return this.users;
-    }
+  createUser(user: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarried: boolean;
+  }) {
+    this.users.push(user);
+    return this.users;
+  }
 }
